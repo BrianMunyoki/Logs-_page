bd04fdcf54ab5a78cc2357a43c1b66deddaaa23d
Brian Munyoki <brianmunyoki168@gmail.com>
2026-04-19 02:33:01 +0300

    working on post crud

---
+++ public/index.js
@@ -0,0 +1,39 @@
+const form =document.getElementById("logForm");
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+        const logType = document.querySelector('input[name="logType"]:checked')?.value;
+        const workType = document.querySelector('input[name="workType"]:checked')?.value;
+        const project = document.getElementById("project").value;
+        const fromTime = document.getElementById("fromTime").value;
+        const toTime = document.getElementById("toTime").value;
+        const task = document.getElementById("task").value;
+        const message = document.getElementById("message").value;
+        
+        const logData = {
+        logType,
+        project,
+        workType,
+        fromTime,
+        toTime,
+        task,
+        message
+    };
+    fetch("http://localhost:3000/Logtype",{ 
+        method:"POST",
+        headers:{"Content-Type": "application/json"},
+        body: JSON.stringify(logData)})
+    
+    .then(response=>{
+        if(!response.ok){
+            throw new Error("Failed to save log");
+        }
+        return response.json();
+    })
+    .then(data=>{
+        (data);
+    })
+    .catch(error=>{
+        console.error(error);
+    })
+    })
