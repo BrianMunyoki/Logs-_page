5bd5b8474aa0576afebae3297714b70788927020
Brian Munyoki <brianmunyoki168@gmail.com>
2026-05-06 01:41:12 +0300

    working on drop down , filter and fetch

--- public/index.js
+++ public/index.js
@@ -39,23 +39,33 @@ form.addEventListener("submit", (e)=>{
     })
     })
     }
-const filters=document.querySelector(".filters");
+
+const filterButton=document.querySelector("#filterButton");
+
+filterButton.addEventListener("click",()=>{
+  const project=document.querySelector("#project").value;
+  const fromtime=document.querySelector("#fromTime").value;
+  const totime=document.querySelector("#toTime").value;
+
+console.log(project, fromtime, toTime);
 
   fetch("http://localhost:3000/Logtype")
   .then(response => response.json())
   .then(data => {
     console.log("data received", data);
+    const filtered=data.filter(item=>item.project===project);
       const tbody=document.querySelector("#logsTable");
       tbody.innerHTML = ""; //clearing the table. 
-      data.forEach(item=>{
+      filtered.forEach(item=>{
         const tr=document.createElement("tr");
         tr.innerHTML=`
         <td>${item.project} </td>
-        <td>${item["From time"]}</td>
-        <td>${item.Message} </td>
-        <td>${item["task number"]}</td>
+        <td>${item.fromtime}</td>
+        <td>${item.toTime}</td>
+        <td>${item.message} </td>
         <td>${item.id}</td>
         `
+        tbody.appendChild(tr);
       })
       console.log(tbody)
   })
@@ -63,3 +73,4 @@ const filters=document.querySelector(".filters");
     console.error(error);
   });
 
+})
